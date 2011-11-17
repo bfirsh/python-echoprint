dc7e52d9cca29eff5e8ee51aa6a7e7951b31ced8
Yves Raimond <yves.raimond@bbc.co.uk>
2011-11-17 10:17:18 +0000

    Fixing example script to take into account start and duration

--- examples/identify.py
+++ examples/identify.py
@@ -6,13 +6,13 @@ import sys
 import struct
 
 if len(sys.argv) < 4:
-    print "Usage: identify.py api_key filename start end"
+    print "Usage: identify.py api_key filename start duration"
     sys.exit(1)
 
 api_key = sys.argv[1]
 filename = sys.argv[2]
 start = sys.argv[3]
-end = sys.argv[4]
+duration = sys.argv[4]
 
 p = subprocess.Popen([
     'ffmpeg',
@@ -20,8 +20,8 @@ p = subprocess.Popen([
     '-ac', '1',
     '-ar', '11025',
     '-f', 's16le',
-    '-t', '30',
-    '-ss', '0',
+    '-t', duration,
+    '-ss', start,
     '-',
 ], stdout=subprocess.PIPE)
 
@@ -34,6 +34,7 @@ while True:
     samples.append(struct.unpack('h', sample)[0] / 32768.0)
 
 d = echoprint.codegen(samples, int(start))
+print d['code']
 
 d['api_key'] = api_key
 res = urllib.urlopen('http://developer.echonest.com/api/v4/song/identify?' + urllib.urlencode(d)).read()
